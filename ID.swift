2d9f51653ae9a3248e8d6f3b842c9061d4915b8d
petriz_f <petriz_f@epitech.eu>
2015-02-26 14:56:24 +0100

    add new entitie

--- ID.swift
+++ ID.swift
@@ -14,4 +14,17 @@ class ID: NSManagedObject {
     @NSManaged var log: String
     @NSManaged var pass: String
     
+    class func createInManagedObjectContext(moc: NSManagedObjectContext, log: String, pass: String) -> ID {
+        let newItem = NSEntityDescription.insertNewObjectForEntityForName("ID", inManagedObjectContext: moc) as ID
+        newItem.log = log
+        newItem.pass = pass
+        
+        return newItem
+    }
+    
+}
+
+class SEMESTER: NSManagedObject {
+    
+    @NSManaged var title: String
 }
