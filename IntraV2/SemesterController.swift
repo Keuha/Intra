e30d322baf473b6fa4afc713ec7eec53a58db343
petriz_f <petriz_f@epitech.eu>
2015-02-26 18:28:53 +0100

    Allow to select which semester to display information for

---
+++ IntraV2/SemesterController.swift
@@ -0,0 +1,85 @@
+//
+//  SemesterController.swift
+//  IntraTek
+//
+//  Created by Franck PETRIZ on 26/02/2015.
+//  Copyright (c) 2015 Keuha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class SemesterViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var semesterTableView: UITableView!
+    var activityIndicator = UIActivityIndicatorView()
+    var appDel : AppDelegate!
+    var context : NSManagedObjectContext!
+    var CookieManager : Cookie!
+
+    
+    override func viewDidLoad() {
+        
+        CookieManager = CookieState.CookieManager
+        appDel = (UIApplication.sharedApplication().delegate as AppDelegate)
+        context = appDel!.managedObjectContext
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.saveButton.enabled = false
+        self.navigationController?.navigationBar.hidden = false
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("semesterCell") as CustomSemesterCell
+        cell.semesterLabel.text = "Semestre : \(indexPath.row)"
+        println("CookieManager.semester \(CookieManager.semester) contains \(indexPath.row)")
+        if contains(CookieManager.semester, "\(indexPath.row)") {
+            println("YEAH")
+            cell.registerLabel.text = "\u{2713}"
+        } else {
+             cell.registerLabel.text = "+"
+        }
+        return cell
+    }
+   
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let cell = tableView.cellForRowAtIndexPath(indexPath) as CustomSemesterCell
+        if  cell.registerLabel.text == "\u{2713}" {
+            cell.registerLabel.text = "+"
+            let ind : Int = find(CookieManager.semester, "\(indexPath.row)")!
+            CookieManager.semester.removeAtIndex(ind)
+        } else {
+            cell.registerLabel.text = "\u{2713}"
+            CookieManager.semester.insert("\(indexPath.row)", atIndex: CookieManager.semester.count)
+        }
+        if self.saveButton.enabled == false {
+            self.saveButton.enabled = true
+        }
+    }
+    
+    @IBAction func SaveButtonCliked(sender: AnyObject) {
+        var request = NSFetchRequest(entityName:"SEMESTER")
+        var result: NSArray = context.executeFetchRequest(request, error:nil) as [SEMESTER]
+        for res in result {
+            context.deleteObject(res as NSManagedObject)
+        }
+        for e in CookieManager.semester {
+        var newID = NSEntityDescription.insertNewObjectForEntityForName("SEMESTER", inManagedObjectContext: self.context!) as NSManagedObject
+        newID.setValue(e, forKey:"title")
+        }
+        CookieManager?.refreshWeekPlanning()
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    
+}
+
